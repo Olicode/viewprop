e85087586a0014ed8cf1cbd3e9c0a9a9d6cecb0f
Shania Tsamara <shaniatsamara@gmail.com>
2021-12-07 17:46:31 +0800

    did the skeleton of the seller tab

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
-  
+
   def home
+  end
+
+  def dashboard
+    @my_listings = Listing.where(user: current_user)
+    @user = current_user
+    # @bookings_to_seller = Booking.joins(:listing).where(listing: { user: current_user })
+    # @pending_seller_bookings = @bookings_to_seller.where(status: "Pending")
+    # @current_seller_bookings = @bookings_to_seller.where(status: ["Accepted", "Rejected"])
+    # @bookings_i_made = current_user.bookings
   end
 end
