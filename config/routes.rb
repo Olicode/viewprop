53cd14af26e1cc983816011838d46ecda0c160ff
Brian Koekkoek <bjkoekkoek@hotmail.com>
2021-12-07 17:28:43 +0800

    All CRUD routes implemented (except destroy) with basic view template

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :listings, only: %i[index show]
+  resources :listings, only: %i[index show new create edit update]
 end
