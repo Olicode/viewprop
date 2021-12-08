ed3d44e21f57167282c7f026fc7305036ff6ea65
Shania Tsamara <shaniatsamara@gmail.com>
2021-12-08 15:06:27 +0800

    created booking view and added custom validations

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,29 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+    @listing = Listing.find(params[:listing_id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @listing = Listing.find(params[:listing_id])
+    @booking.listing = @listing
+    @booking.user = current_user
+    if @booking.save
+      redirect_to dashboard
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy!
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :start_time, :end_time)
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,20 @@ class Booking < ApplicationRecord
   belongs_to :user
 
   validates :date, :start_time, :end_time, presence: true
+  validate :end_time_cannot_be_less_than_start_time, :different_owner
 
   STATUS = ["Accepted, Rejected", "Pending"]
+
+  def end_time_cannot_be_less_than_start_time
+    return unless start_time && end_time
+    if end_time < start_time
+      errors.add(:end_time, "has to be after the starting time")
+    end
+  end
+
+  def different_owner
+    if listing.user == user
+      errors.add(:user, "You cannot book to view your own listing")
+    end
+  end
 end
