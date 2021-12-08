3342f3c05c08b48851fcab22ac40ce67b2841844
Shania Tsamara <shaniatsamara@gmail.com>
2021-12-08 14:56:37 +0800

    added the validations and destroy dependents

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :listing
   belongs_to :user
 
+  validates :date, :start_time, :end_time, presence: true
+
   STATUS = ["Accepted, Rejected", "Pending"]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   resources :listings, only: %i[index show new create edit update] do
     resources :offers, only: %i[new create]
+    resources :bookings, only: %i[new create]
   end
   resources :offers, only: %i[edit update]
+  resources :bookings, only: :delete
 end
