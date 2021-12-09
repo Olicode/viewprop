6b1b6464f4d9026c6b141c0f82e121169df7c9b8
Shania Tsamara <shaniatsamara@gmail.com>
2021-12-09 12:46:32 +0800

    made the validations for offer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -4,7 +4,7 @@ class Offer < ApplicationRecord
 
   validates :final_price, presence: true
   validates :final_price, numericality: { greater_than_or_equal_to: 0 }
-  validate :cannot_make_offer_on_own_listing, :two_offers_on_listing
+  validate :cannot_make_offer_on_own_listing, :two_offers_on_listing, :no_offer_on_sold_listing
 
   def cannot_make_offer_on_own_listing
     if user == listing.user
@@ -15,6 +15,12 @@ class Offer < ApplicationRecord
   def two_offers_on_listing
     if listing.offers.where.not(buyer_confirmation: false, seller_confirmation: false).pluck(:user_id).include?(user_id)
       errors.add(:user, "You cannot place two offers on the same listing")
+    end
+  end
+
+  def no_offer_on_sold_listing
+    if listing.sold == true
+      errors.add(:user, "You cannot make an offer")
     end
   end
 end
