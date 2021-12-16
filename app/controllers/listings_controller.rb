c04e8657b9d03e0ab633ed3f669d5b550215ed41
Shania Tsamara <shaniatsamara@gmail.com>
2021-12-16 17:37:51 +0800

    took out the show authentication

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,5 @@
 class ListingsController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[index show]
+  skip_before_action :authenticate_user!, only: %i[index]
   before_action :set_listing, only: %i[show edit update]
 
   def index
